2d9d9c7370220bd8a81322d423331f7cc9f705c1
Marcelo <marceloaugusto10123@gmail.com>
2023-10-26 16:50:04 -0300

    update

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -3,7 +3,5 @@ import Style from './style';
 
 export default function Link() {
     const URL = 'https://www.linkedin.com/in/alessandro-l-menezes-57906b71/';
-    return (
-        <></>
-    );
+
 }
